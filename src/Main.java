ec0ff8f59212f19fd1f257ac4a5c6081e33af0e8
Sora456 <tarcisiusalonso@gmail.com>
2023-06-02 09:56:06 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+        Pessoa cliente;
+        cliente = new Pessoa();
+        Pessoa cpf;
+        cpf = new Pessoa();
+        Pessoa RG;
+        RG = new Pessoa();
+        Pessoa Nome;
+        Nome = new Pessoa();
+        Pessoa telefone;
+        telefone = new Pessoa();
+
+        
+
+
+
+        Piloto Piloto = new Piloto();
+        Balao BalaoVoo = new Balao();
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,6 @@
+public class Pessoa {
+    String client = "1";
+    String cpf = ("000.000.000.000");
+
+
+}
